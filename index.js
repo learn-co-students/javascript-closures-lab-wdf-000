d4719445080a5bc8e313f24debf91c2d8c1024ef
irevived1 <haifeng.wu@jjay.cuny.edu>
2016-10-01 13:15:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return { addBump, getBumps };
+}
+
+function createAnimal(animalType) {
+  return function deadlyDevice(weptype) {
+    return { animalType: `${animalType}`, deadlyDevice: `${weptype}` };
+  }
+}
+
+var sharkCreator = createAnimal('Shark');
+
+var sharkWithFrickinCannon = sharkCreator('Cannon');
+var sharkWithFrickinLaserbeam  = sharkCreator('Laserbeam');
