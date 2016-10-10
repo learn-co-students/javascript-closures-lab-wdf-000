f05b7bc828f9fc19d8ed6c5caaf5fe5f9d5be7ac
satub <satu.barnhill@gmail.com>
2016-10-10 14:38:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+///// Austin Powers' bumpCounter
+function bumpCounter(){
+  var counter = 0;
+
+  function addBump(){
+    counter ++;
+  }
+
+  function getBumps(){
+    return counter;
+  }
+
+  return {addBump, getBumps};
+}
+
+///// Dr. Evil's animal factory
+function createAnimal(animalType){
+  return function (deadlyDevice){
+    return {animalType: animalType, deadlyDevice: deadlyDevice};
+  };
+}
+
+var sharkCreator = createAnimal("Shark");
+
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam");
+
+var sharkWithFrickinCannon = sharkCreator("Cannon");
