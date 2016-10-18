c6cd617e43923c54537db75199657269c2625e74
ckshei <cks856@gmail.com>
2016-10-18 18:34:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const app = "I don't do much."
+function bumpCounter() {
+  let counter = 0;
+  function addBump() {
+    return counter += 1;
+  }
+  function getBumps() {
+    return counter;  
+  }
+  
+  return {
+    addBump, getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice}
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+
+var sharkWithFrickinLaserBeam = sharkCreator('Cannon')
